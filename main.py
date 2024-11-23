0a7a129e5472fcf9d81e9a84e4f8ddc10ab98cb5
RodionClepa <raduclepa@gmail.com>
2024-11-23 16:27:24 +0200

    Add config and fix bugs

--- main.py
+++ main.py
@@ -33,77 +33,83 @@ class NodeRole(Enum):
     FOLLOWER = "Follower"
 
 class Node:
-    def __init__(self, ip, port, name):
+    def __init__(self, ip, port, name, range_response, range_await):
         self.ip = ip
         self.port = port
         self.name = name
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_socket.bind((self.ip, self.port))
-        print(f"{get_timestamp()} - Node {self.name} is on {self.ip}:{self.port}")
         self.role = NodeRole.FOLLOWER
-        self.last_heartbeat_time = time.time()
         self.votes_received = 0
         self.term = 0
-        self.patience = random.randint(30, 60)
+        self.patience = random.randint(range_await[0], range_await[1])
+        self.response = random.randint(range_response[0], range_response[1])
+        self.last_heartbeat_time = time.time()
+        print(f"{get_timestamp()} - Node {self.name} is on {self.ip}:{self.port}, pat {self.patience}, res {self.response}")
 
     def listen_for_messages(self):
         while True:
-            message, address = self.server_socket.recvfrom(4096)  # Buffer size of 4096 bytes
-            d_message = message.decode().split("|")
-            action = d_message[0]
-            sender_name = d_message[1]
-            rest = " ".join(d_message[2:])
-            sender_port = address[1]
-            print(f"{get_timestamp()} - Node {self.name} received {action}: '{rest}' from {sender_name} | {sender_port}")
-            
-            if action == "heartbeat":
-                # Update last_heartbeat_time when a heartbeat is received
+            try:
+                message, address = self.server_socket.recvfrom(4096)  # Buffer size of 4096 bytes
+                d_message = message.decode().split("|")
+                action = d_message[0]
+                sender_name = d_message[1]
+                rest = " ".join(d_message[2:])
+                sender_port = address[1]
+                print(f"{get_timestamp()} - Node {self.name} got action:{action}: '{rest}' from {sender_name}|{sender_port}")
                 self.last_heartbeat_time = time.time()
-                print(f"{get_timestamp()} - Node {self.name} received heartbeat.")
-
-                # Stop the election and become a follower if heartbeat from leader
-                if self.role == NodeRole.CANDIDATE:
-                    print(f"{get_timestamp()} - Node {self.name} received heartbeat. Stopping election and following {sender_name}.")
-                    self.role = NodeRole.FOLLOWER
-                    self.votes_received = 0  # Reset vote count
-                    self.term += 1  # Increment term as leader heartbeat has arrived
-
-                elif self.role == NodeRole.LEADER:
-                    print(f"{get_timestamp()} - Node {self.name} received heartbeat. Stop being leader {sender_name}.")
-                    self.role = NodeRole.FOLLOWER
-                    self.votes_received = 0  # Reset vote count
-                    self.term += 1  # Increment term as leader heartbeat has arrived
+                if action == "heartbeat":
+                    # Update last_heartbeat_time when a heartbeat is received
                     self.last_heartbeat_time = time.time()
+                    print(f"{get_timestamp()} - Node {self.name} received heartbeat.")
 
-                # Respond back to server
-                response_message = f"heartbeat_ack|{self.name}|Heartbeat acknowledged"
-                self.server_socket.sendto(response_message.encode(), address)
-                print(f"{get_timestamp()} - Node {self.name} sent heartbeat acknowledgment to {sender_name}")
-            
-            elif action == "RequestVote":
-                # Handle vote request during election
-                print(f"{get_timestamp()} - Node {self.name} received RequestVote from {sender_name}.")
-                if self.role == NodeRole.FOLLOWER:
-                    print("rest", rest, int(rest), self.term <= int(rest))
-                    if self.term <= int(rest):
-                        print(f"{get_timestamp()} - Node {self.name} voted for {sender_name}")
-                        self.send_vote(sender_name, address)
-                    else:
+                    # Stop the election and become a follower if heartbeat from leader
+                    if self.role == NodeRole.CANDIDATE:
+                        print(f"{get_timestamp()} - Node {self.name} received heartbeat. Stopping election and following {sender_name}.")
+                        self.role = NodeRole.FOLLOWER
+                        self.votes_received = 0  # Reset vote count
+                        self.term += 1  # Increment term as leader heartbeat has arrived
+
+                    elif self.role == NodeRole.LEADER:
+                        print(f"{get_timestamp()} - Node {self.name} received heartbeat. Stop being leader by {sender_name}.&&&&&&&&&&&&&&&&&&&")
+                        self.role = NodeRole.FOLLOWER
+                        self.votes_received = 0  # Reset vote count
+                        self.term += 1  # Increment term as leader heartbeat has arrived
+
+                    # Respond back to server
+                    response_message = f"heartbeat_ack|{self.name}|Heartbeat acknowledged"
+                    self.server_socket.sendto(response_message.encode(), address)
+                    print(f"{get_timestamp()} - Node {self.name} sent heartbeat acknowledgment to {sender_name}")
+
+                elif action == "heartbeat_ack":
+                    print(f"{get_timestamp()} - Node {self.name} recieved heartbeat acknowledgment from {sender_name}")
+                
+                elif action == "RequestVote":
+                    # Handle vote request during election
+                    print(f"{get_timestamp()} - Node {self.name} received RequestVote from {sender_name}.")
+                    if self.role == NodeRole.FOLLOWER:
+                        if self.term <= int(rest):
+                            print(f"{get_timestamp()} - Node {self.name} voted for {sender_name}")
+                            self.send_vote(sender_name, address)
+                        else:
+                            print(f"{get_timestamp()} - Node {self.name} ignored vote request as election is ongoing.")
+                    elif self.role == NodeRole.CANDIDATE:
                         print(f"{get_timestamp()} - Node {self.name} ignored vote request as election is ongoing.")
-                elif self.role == NodeRole.CANDIDATE:
-                    print(f"{get_timestamp()} - Node {self.name} ignored vote request as election is ongoing.")
-            
-            elif action == "VoteGranted":
-                print(f"{get_timestamp()} - Node {self.name} received VoteGranted from {sender_name}.")
-                self.votes_received += 1
-                if self.votes_received > len(list_nodes) // 2:  # Majority votes
-                    print(f"{get_timestamp()} - Node {self.name} has won the election and is now the LEADER.")
-                    self.role = NodeRole.LEADER
-                    self.votes_received = 0  # Reset vote count for new leader state
 
-            else:
-                print(f"{get_timestamp()} - Node {self.name} received unknown message.")
+                elif action == "VoteGranted":
+                    print(f"{get_timestamp()} - Node {self.name} received VoteGranted from {sender_name}.")
+                    self.votes_received += 1
+                    if self.votes_received > len(list_nodes) // 2:  # Majority votes
+                        print(f"{get_timestamp()} - Node {self.name} has won the election and is now the LEADER.********************")
+                        self.role = NodeRole.LEADER
+                        self.votes_received = 0  # Reset vote count for new leader state
 
+                else:
+                    print(f"{get_timestamp()} - Node {self.name} received unknown message.")
+
+            except socket.timeout:
+                # Handle the timeout gracefully
+                print(f"{get_timestamp()} - Node {self.name} did not receive any messages within the timeout period.")
 
     def send_vote(self, sender_name, address):
         # Respond to vote request by granting vote
@@ -133,44 +139,41 @@ class Node:
 
     def select_target(self):
         available_ports = [port for port in list_nodes if port != self.port]
-    
-        # Choose a random port from the remaining ports
-        if available_ports:
-            return ('127.0.0.1', random.choice(available_ports))
-        else:
-            raise ValueError("No other ports available to select from.")
-
+        return available_ports
 
     def sent_heartbeat(self):
         while True:
-            # Send a heartbeat message
+            if self.role != NodeRole.LEADER:
+                continue
+            # Send a heartbeat message to all other nodes
             message = f"heartbeat|{self.name}|{get_random_message()}"
-            target = self.select_target()
-            self.server_socket.sendto(message.encode(), target)
-            print(f"{get_timestamp()} - Node {self.name} sent heartbeat to {target}")
-
-            # Wait for a response (acknowledgment) from the follower
-            try:
-                self.server_socket.settimeout(2)  # Wait for 2 seconds for a response
-                response, address = self.server_socket.recvfrom(4096)  # Buffer size of 4096 bytes
-                print(f"{get_timestamp()} - Node {self.name} received response: {response.decode()} from {address}")
-            except socket.timeout:
-                print(f"{get_timestamp()} - Node {self.name} did not receive heartbeat ack from follower, retrying...")
+            
+            # Get the list of all other nodes
+            targets = self.select_target()
+            
+            # Send the heartbeat to all nodes
+            for target_port in targets:
+                target = ('127.0.0.1', target_port)
+                self.server_socket.sendto(message.encode(), target)
+                print(f"{get_timestamp()} - Node {self.name} sent heartbeat to {target}")
             
             # Wait for a bit before sending the next heartbeat
-            time.sleep(5)
+            time.sleep(self.response)
     
+
     def start(self):
-        # Start the listening thread
-        if (self.role == NodeRole.FOLLOWER):
-            node_thread = threading.Thread(target=self.listen_for_messages)
+        # Create a thread to send heartbeats
+        heartbeat_thread = threading.Thread(target=self.sent_heartbeat, daemon=True)
+        heartbeat_thread.start()
 
-            # Check if the follower has stopped receiving heartbeats
-            threading.Thread(target=self.check_heartbeat_timeout).start()
-        elif (self.role == NodeRole.LEADER):
-            node_thread = threading.Thread(target=self.sent_heartbeat)
+        # Create a thread to listen for messages
+        listen_thread = threading.Thread(target=self.listen_for_messages, daemon=True)
+        listen_thread.start()
+        # Check if the follower has stopped receiving heartbeats
+        self.last_heartbeat_time = time.time()
+        threading.Thread(target=self.check_heartbeat_timeout).start()
 
-        node_thread.start()
+        
 
     def set_role(self, new_role: NodeRole):
         # Method to change the role of the node
@@ -183,37 +186,63 @@ class Node:
     def check_heartbeat_timeout(self):
         # Method to check if the follower has stopped receiving heartbeats
         while True:
-            if self.role != NodeRole.LEADER and time.time() - self.last_heartbeat_time > self.patience:
-                print(f"{get_timestamp()} - Node {self.name} stopped receiving heartbeats. Leader may have failed!")
+            if self.role == NodeRole.FOLLOWER and (time.time() - self.last_heartbeat_time) > self.patience:
+                print(f"{get_timestamp()} - Node {self.name} ({time.time() - self.last_heartbeat_time}) stopped receiving heartbeats. Leader may have failed!!!!!!!!!!!!!!!!!!!1")
                 self.start_election()
             time.sleep(1)
 
 
 global list_nodes
-list_nodes = [5000, 5001, 5002, 5003, 5004, 5005 ]
 
-node_1 = Node('127.0.0.1', list_nodes[0], "Leader")
-node_1.set_role(NodeRole.LEADER)
+nicknames = [
+    "Ace", "Buzz", "Champ", "Sparky", "Tiger", "Sparky", "Munchkin", "Cookie", 
+    "Bear", "Doodle", "Rocket", "Bubbles", "Gizmo", "Jester", "Shadow", "Peanut", 
+    "Zippy", "Sunshine", "Snickers", "Cuddlebug", "Pookie", "Snuffy", "Twinkle", 
+    "Squirt", "Cookie Monster", "Bigfoot", "Taz", "Sweets", "Buddy", "Jellybean"
+]
 
-node_2 = Node('127.0.0.1', list_nodes[1], "Follower")
-node_2.set_role(NodeRole.FOLLOWER)
+def get_random_nick():
+    return random.choice(nicknames)
 
-node_3 = Node('127.0.0.1', list_nodes[2], "Ion")
-node_3.set_role(NodeRole.FOLLOWER)
+range_response = [10, 16]
+range_await = [8, 13]
+number_nodes = 4
+list_nodes = []
+port_number = 5000
+leader_number = random.randint(0, number_nodes - 1)
+node_list = []
+for i in range(0, number_nodes):
+    list_nodes.append(port_number + i)
+    node = Node('127.0.0.1', port_number + i, get_random_nick(), range_response, range_await)
+    if i == leader_number:
+        node.set_role(NodeRole.LEADER)
+    node_list.append(node)
 
-node_4 = Node('127.0.0.1', list_nodes[3], "Third")
-node_4.set_role(NodeRole.FOLLOWER)
+for node in node_list:
+    node.start()
 
-node_5 = Node('127.0.0.1', list_nodes[4], "Five")
-node_5.set_role(NodeRole.FOLLOWER)
+# node_1 = Node('127.0.0.1', list_nodes[0], "Leader")
+# node_1.set_role(NodeRole.LEADER)
 
-node_6 = Node('127.0.0.1', list_nodes[5], "Six")
-node_6.set_role(NodeRole.FOLLOWER)
+# node_2 = Node('127.0.0.1', list_nodes[1], "Follower")
+# node_2.set_role(NodeRole.FOLLOWER)
 
-node_1.start()
-node_2.start()
-node_3.start()
-node_4.start()
-node_5.start()
-node_6.start()
+# node_3 = Node('127.0.0.1', list_nodes[2], "Ion")
+# node_3.set_role(NodeRole.FOLLOWER)
+
+# node_4 = Node('127.0.0.1', list_nodes[3], "Third")
+# node_4.set_role(NodeRole.FOLLOWER)
+
+# node_5 = Node('127.0.0.1', list_nodes[4], "Five")
+# node_5.set_role(NodeRole.FOLLOWER)
+
+# node_6 = Node('127.0.0.1', list_nodes[5], "Six")
+# node_6.set_role(NodeRole.FOLLOWER)
+
+# node_1.start()
+# node_2.start()
+# node_3.start()
+# node_4.start()
+# node_5.start()
+# node_6.start()
 
